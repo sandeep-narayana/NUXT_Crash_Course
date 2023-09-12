5c2bb1abfd8f6b2870f06a7c1b3ce497d7fa0f0e
Sandeep Rawat <132456591+sandeep-narayana@users.noreply.github.com>
2023-09-12 13:54:18 +0530

    how to use links - use NuxtLink instead of href

--- nuxt_app/pages/index.vue
+++ nuxt_app/pages/index.vue
@@ -1,4 +1,14 @@
 <template>
+  <header>
+    <NuxtLink to="/">Nuxt logo</NuxtLink>
+    <nav>
+      <ul>
+        <li><NuxtLink to="/">Home</NuxtLink></li>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+        <li><NuxtLink to="/products">Products</NuxtLink></li>
+      </ul>
+    </nav>
+  </header>
   <h2>Home Page</h2>
   <p>
     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum tempora,
@@ -7,8 +17,6 @@
     unde asperiores.
   </p>
 </template>
-
-<script></script>
 
 <style scoped>
 h2 {
