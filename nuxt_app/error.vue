4f0fe3a7ff48836b013c11d1cbcd998b23b00075
Sandeep Rawat <132456591+sandeep-narayana@users.noreply.github.com>
2023-09-12 18:03:27 +0530

    how to use error page

---
+++ nuxt_app/error.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="error-page">
+      <p class="error-message">{{ error.message }}</p>
+      <p class="error-status">{{ error.statusCode }}</p>
+      <button class="go-home-button" v-on:click="HandelClearError()">Go to Home</button>
+    </div>
+  </template>
+  
+  <script setup>
+  const { error } = defineProps(['error']);
+
+  const HandelClearError =() => clearError({redirect:'/'})
+  </script>
+  
+  <style scoped>
+  .error-page {
+    text-align: center;
+    padding: 20px;
+  }
+  
+  .error-message {
+    font-size: 24px;
+    color: red;
+  }
+  
+  .error-status {
+    font-size: 18px;
+    color: darkred;
+  }
+  
+  .go-home-button {
+    background-color: #007bff;
+    color: white;
+    padding: 10px 20px;
+    border: none;
+    cursor: pointer;
+  }
+  
+  .hello-text {
+    margin-top: 20px;
+    font-size: 18px;
+  }
+  </style>
+  
--- nuxt_app/pages/products/[id].vue
+++ nuxt_app/pages/products/[id].vue
@@ -14,6 +14,10 @@ const url = `https://fakestoreapi.com/products/${id}`;
 
 // fetch the product
 const { data: product } = await useFetch(url, { key: id });
+
+if(!product.value) {
+  throw createError({statusCode:404, statusMessage: 'Product not found', fatal: true})
+}
 definePageMeta({
   layout: "product",
 });
