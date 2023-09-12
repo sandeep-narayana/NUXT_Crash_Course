c4f4b42ba0d1fd7e7842980aba28066386ff987a
Sandeep Rawat <132456591+sandeep-narayana@users.noreply.github.com>
2023-09-12 12:50:31 +0530

    Route Parameters

---
+++ nuxt_app/pages/products/[id].vue
@@ -0,0 +1,14 @@
+<!-- /products/:id -->
+
+<template>
+    <div>
+        <p>Product Details for {{ id }}</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, ipsam.</p>
+    </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params
+</script>
+
+<style scoped></style>
---
+++ nuxt_app/pages/products/index.vue
@@ -0,0 +1,11 @@
+<!-- http://localhost:3000/products/ -->
+
+<template>
+    <div>
+        <p>Product</p>
+    </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
